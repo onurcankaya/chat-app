d55c23eb6e943344f9575a42295c3b1052e376c7
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-18 11:40:48 +0100

    added eslint prettier config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div>
-      Chat App
-    </div>
-  );
+  return <div>Chat App</div>
 }
 
-export default App;
+export default App
